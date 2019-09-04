a728236588cad9a0933641c18ec64c0b68a49f30
Feliperony <rony.alvessobral@hotmail.com>
2019-09-04 20:12:50 -0300

    exercicio5

---
+++ dist/exercicio5.js
@@ -0,0 +1,4 @@
+var largura = 8;
+var comprimento = 5;
+var area = function (largura, comprimento) { return largura * comprimento; };
+console.log("A \u00C0rea \u00E9: " + area(largura, comprimento));
---
+++ exercicio5.ts
@@ -0,0 +1,7 @@
+let largura: number = 8
+let comprimento: number = 5
+
+let area = (largura:number,comprimento:number) => largura * comprimento
+
+console.log(`A Àrea é: ${area(largura,comprimento)}`)
+
